d6666020106639a1a452a24d934a85d4f13a7993
Mehrdad Dindar <mehrdad.replay@gmail.com>
2021-09-06 16:10:57 +0430

    Some change for seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(UsersTableSeeder::class);
         $this->call(CitiesTableSeeder::class);
         $this->call(Commitment_CeilingsTableSeeder::class);
         $this->call(Fire_commitment_ceilingsTableSeeder::class);
         $this->call(Phone_brandsTableSeeder::class);
         $this->call(Phone_modelsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
