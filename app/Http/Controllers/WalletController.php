1db8d3d596c5b6f1db0f1f1e8ab66b52f056ef3a
Mehrdad Dindar <mehrdad.replay@gmail.com>
2021-09-06 18:01:11 +0430

    Manage sample data for add  wallet

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wallet;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class WalletController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('profile.wallet')->with([
+            'user'      =>      auth()->user(),
+        ]);
+    }
+
+    public function increase(Request $request)
+    {
+        $wallet = Wallet::where('user_id' , "=" ,auth()->id())->first();
+        $wallet_val = $wallet->value;
+        $new_wallet_val = 0;
+        if ($wallet_val != "0"){
+            $new_wallet_val = (int)Crypt::decryptString($wallet->value) + (int)$request['price'];
+        }else{
+            $new_wallet_val = (int)$request['price'];
+        }
+
+        $new_wallet_val = Crypt::encryptString($new_wallet_val);
+        /*$wallet->value = Crypt::encryptString($new_wallet_val);*/
+        Wallet::update([
+            'value' => Crypt::encryptString($new_wallet_val),
+            'title' => $request['title'],
+        ]);
+
+    }
 }
