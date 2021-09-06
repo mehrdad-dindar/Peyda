ac5f0828b2308e3e2d727f37a2986b1047b6aeb5
Mehrdad Dindar <mehrdad.replay@gmail.com>
2021-09-06 16:16:52 +0430

    Add route to Wallet Charge View

--- resources/views/profile/layouts/header.blade.php
+++ resources/views/profile/layouts/header.blade.php
@@ -9,7 +9,7 @@
                 <span class="svg-icon svg-icon-2x mt-1">
 										<svg xmlns="http://www.w3.org/2000/svg"
                                              xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
-                                             viewBox="0 0 24 24" version="1.1">
+                                             viewBox="0 0 24 24">
 											<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
 												<rect x="0" y="0" width="24" height="24"/>
 												<rect fill="#000000" x="4" y="5" width="16" height="3" rx="1.5"/>
@@ -97,7 +97,7 @@
                         <div class="menu menu-sub menu-sub-dropdown menu-column w-250px w-lg-325px" data-kt-menu="true">
                             <!--begin::Heading-->
                             <div class="d-flex flex-column flex-center bgi-no-repeat rounded-top px-9 py-10"
-                                 style="background-image:url('profile/media/misc/pattern-1.jpg')">
+                                 style="background-image:url('{{ URL::asset('profile/media/misc/pattern-1.jpg')}}')">
                                 <!--begin::Title-->
                                 <h3 class="text-white fw-bold mb-3">Quick Links</h3>
                                 <!--end::Title-->
@@ -110,14 +110,13 @@
                             <div class="row g-0">
                                 <!--begin:Item-->
                                 <div class="col-6">
-                                    <a href="../../demo6/dist/pages/projects/budget.html"
+                                    <a href="#"
                                        class="d-flex flex-column flex-center h-100 p-6 bg-hover-light border-end border-bottom">
                                         <!--begin::Svg Icon | path: icons/duotone/Shopping/Euro.svg-->
                                         <span class="svg-icon svg-icon-3x svg-icon-primary mb-2">
 																<svg xmlns="http://www.w3.org/2000/svg"
                                                                      xmlns:xlink="http://www.w3.org/1999/xlink"
-                                                                     width="24px" height="24px" viewBox="0 0 24 24"
-                                                                     version="1.1">
+                                                                     width="24px" height="24px" viewBox="0 0 24 24">
 																	<g stroke="none" stroke-width="1" fill="none"
                                                                        fill-rule="evenodd">
 																		<rect x="0" y="0" width="24" height="24"/>
@@ -138,12 +137,12 @@
                                 <!--end:Item-->
                                 <!--begin:Item-->
                                 <div class="col-6">
-                                    <a href="../../demo6/dist/pages/projects/settings.html"
+                                    <a href="#"
                                        class="d-flex flex-column flex-center h-100 p-6 bg-hover-light border-bottom">
                                         <!--begin::Svg Icon | path: icons/duotone/Communication/Mail-attachment.svg-->
                                         <span class="svg-icon svg-icon-3x svg-icon-primary mb-2">
 																<svg xmlns="http://www.w3.org/2000/svg" width="24px"
-                                                                     height="24px" viewBox="0 0 24 24" version="1.1">
+                                                                     height="24px" viewBox="0 0 24 24">
 																	<path
                                                                         d="M14.8571499,13 C14.9499122,12.7223297 15,12.4263059 15,12.1190476 L15,6.88095238 C15,5.28984632 13.6568542,4 12,4 L11.7272727,4 C10.2210416,4 9,5.17258756 9,6.61904762 L10.0909091,6.61904762 C10.0909091,5.75117158 10.823534,5.04761905 11.7272727,5.04761905 L12,5.04761905 C13.0543618,5.04761905 13.9090909,5.86843034 13.9090909,6.88095238 L13.9090909,12.1190476 C13.9090909,12.4383379 13.8240964,12.7385644 13.6746497,13 L10.3253503,13 C10.1759036,12.7385644 10.0909091,12.4383379 10.0909091,12.1190476 L10.0909091,9.5 C10.0909091,9.06606198 10.4572216,8.71428571 10.9090909,8.71428571 C11.3609602,8.71428571 11.7272727,9.06606198 11.7272727,9.5 L11.7272727,11.3333333 L12.8181818,11.3333333 L12.8181818,9.5 C12.8181818,8.48747796 11.9634527,7.66666667 10.9090909,7.66666667 C9.85472911,7.66666667 9,8.48747796 9,9.5 L9,12.1190476 C9,12.4263059 9.0500878,12.7223297 9.14285008,13 L6,13 C5.44771525,13 5,12.5522847 5,12 L5,3 C5,2.44771525 5.44771525,2 6,2 L18,2 C18.5522847,2 19,2.44771525 19,3 L19,12 C19,12.5522847 18.5522847,13 18,13 L14.8571499,13 Z"
                                                                         fill="#000000" opacity="0.3"/>
@@ -160,14 +159,13 @@
                                 <!--end:Item-->
                                 <!--begin:Item-->
                                 <div class="col-6">
-                                    <a href="../../demo6/dist/pages/projects/list.html"
+                                    <a href="#"
                                        class="d-flex flex-column flex-center h-100 p-6 bg-hover-light border-end">
                                         <!--begin::Svg Icon | path: icons/duotone/Shopping/Box2.svg-->
                                         <span class="svg-icon svg-icon-3x svg-icon-primary mb-2">
 																<svg xmlns="http://www.w3.org/2000/svg"
                                                                      xmlns:xlink="http://www.w3.org/1999/xlink"
-                                                                     width="24px" height="24px" viewBox="0 0 24 24"
-                                                                     version="1.1">
+                                                                     width="24px" height="24px" viewBox="0 0 24 24">
 																	<g stroke="none" stroke-width="1" fill="none"
                                                                        fill-rule="evenodd">
 																		<rect x="0" y="0" width="24" height="24"/>
@@ -188,12 +186,12 @@
                                 <!--end:Item-->
                                 <!--begin:Item-->
                                 <div class="col-6">
-                                    <a href="../../demo6/dist/pages/projects/users.html"
+                                    <a href="#"
                                        class="d-flex flex-column flex-center h-100 p-6 bg-hover-light">
                                         <!--begin::Svg Icon | path: icons/duotone/Communication/Group.svg-->
                                         <span class="svg-icon svg-icon-3x svg-icon-primary mb-2">
 																<svg xmlns="http://www.w3.org/2000/svg" width="24px"
-                                                                     height="24px" viewBox="0 0 24 24" version="1.1">
+                                                                     height="24px" viewBox="0 0 24 24">
 																	<path
                                                                         d="M18,14 C16.3431458,14 15,12.6568542 15,11 C15,9.34314575 16.3431458,8 18,8 C19.6568542,8 21,9.34314575 21,11 C21,12.6568542 19.6568542,14 18,14 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z"
                                                                         fill="#000000" fill-rule="nonzero"
@@ -213,13 +211,13 @@
                             <!--end:Nav-->
                             <!--begin::View more-->
                             <div class="py-2 text-center border-top">
-                                <a href="../../demo6/dist/pages/profile/activity.html"
+                                <a href="#"
                                    class="btn btn-color-gray-600 btn-active-color-primary">View All
                                     <!--begin::Svg Icon | path: icons/duotone/Navigation/Right-2.svg-->
                                     <span class="svg-icon svg-icon-5">
 														<svg xmlns="http://www.w3.org/2000/svg"
                                                              xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
-                                                             height="24px" viewBox="0 0 24 24" version="1.1">
+                                                             height="24px" viewBox="0 0 24 24">
 															<g stroke="none" stroke-width="1" fill="none"
                                                                fill-rule="evenodd">
 																<polygon points="0 0 24 0 24 24 0 24"/>
@@ -281,7 +279,7 @@
                             <!--end::Menu separator-->
                             <!--begin::Menu item-->
                             <div class="menu-item px-5">
-                                <a href="#" class="menu-link px-5">
+                                <a href="{{ route('wallet') }}" class="menu-link px-5">
                                     <span class="menu-text">کیف پول</span>
                                     <span class="menu-badge">
                                         <span class="badge badge-light-danger fw-bolder fs-7">
@@ -434,7 +432,7 @@
                             <span class="svg-icon svg-icon-1">
 													<svg xmlns="http://www.w3.org/2000/svg"
                                                          xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
-                                                         height="24px" viewBox="0 0 24 24" version="1.1">
+                                                         height="24px" viewBox="0 0 24 24">
 														<g stroke="none" stroke-width="1" fill="none"
                                                            fill-rule="evenodd">
 															<rect x="0" y="0" width="24" height="24"/>
