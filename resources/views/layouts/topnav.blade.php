a73563e89b3b485571d08ad91765813a55374ec9
Mehrdad Dindar <mehrdad.replay@gmail.com>
2021-09-21 16:04:34 +0430

    some edit for mobile responsive and persian numbers

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid sticky-top" id="topNav">
-    <div class="row p-2">
-        <div class="col-6 d-flex align-items-center">
+    <div class="row d-none d-md-flex p-2">
+        <div class="col-md-8 d-flex align-items-center">
             <div class="logo">
                 <a href="{{ route('index') }}">
                     <img src="{{ URL::asset('front/img/Logo.png')}}" class="img-fluid" alt="">
@@ -15,14 +15,14 @@
                 </ul>
             </nav>
         </div>
-        <div class="col-6 d-flex align-items-center justify-content-end">
+        <div class="col-md-4 d-flex align-items-center justify-content-end">
             @guest
                     <a href="{{ route('login') }}" class="nav-link">ورود | عضویت</a>
             @else
                 <a id="navbarDropdown" class="nav-link" href="{{route('panel')}}" role="button" v-pre>
                     سلام {{ Auth::user()->getFullNameAttribute() ?? 'کاربر عزیز'  }}
                 </a>
-                <span class="nav-link">|</span>
+                <span class="nav-link separator">|</span>
                 <a href="{{ route('logout') }}" class="nav-link"
                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -35,64 +35,25 @@
             {{--<a href="#" class="nav-link" data-bs-toggle="modal" data-bs-target="#myModal">ورود | عضویت</a>--}}
         </div>
     </div>
-</div>
-
-<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">ورود / عضویت</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="row d-md-none py-2">
+        <div class="col-4 text-start d-flex align-items-center">
+            <div class="nav-toggle d-flex justify-content-center align-items-center">
+                <i class="fas fa-bars fa-lg"></i>
             </div>
-            <div class="modal-body">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="email" class="form-label">ایمیل</label>
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
-                               name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">رمز عبور</label>
-                        <input type="password" class="form-control @error('password') is-invalid @enderror"
-                               id="password" name="password" required autocomplete="current-password">
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                    <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember"
-                                       id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                ورود
-                            </button>
-
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-                        </div>
-                    </div>
-                </form>
+        </div>
+        <div class="col-4 text-center">
+            <div class="mobile-logo">
+                <a href="{{ route('index') }}">
+                    <img src="{{ URL::asset('front/img/Logo.png')}}" class="img-fluid" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-4 text-end ">
+            <div class="menu-search">
+                <i class="fas fa-search fa-lg"></i>
+            </div>
+            <div class="my-account">
+                <i class="fas fa-user fa-lg"></i>
             </div>
         </div>
     </div>
