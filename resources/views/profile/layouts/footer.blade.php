38ff4a82065e036fa729648fb743ddb32b785096
Mehrdad Dindar <mehrdad.replay@gmail.com>
2021-09-28 18:26:53 +0330

    footer changes

--- resources/views/profile/layouts/footer.blade.php
+++ resources/views/profile/layouts/footer.blade.php
@@ -3,18 +3,17 @@
     <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
-            <span class="text-muted fw-bold me-1">{{ '1392 - '.Verta()->year }}©</span>
+            <span class="text-muted fw-bold me-1">{{\Carbon\Carbon::now()->year}} copyright ©</span>
             طراحی، توسعه و دیجیتال مارکتینگ تخصصی
-            <a href="https://nokarto.com" target="_blank" class="text-gray-800 text-hover-primary">شرکت انفورماتیک نوکارتو</a>
+            <a href="https://nokarto.com" target="_blank" class="text-gray-800 text-hover-primary">شرکت انفورماتیک صنعتی نوکارتو</a>
         </div>
         <!--end::Copyright-->
         <!--begin::Menu-->
         <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
             <li class="menu-item">
-                <a href="https://nokarto.com/%d9%85%d8%ac%d9%85%d9%88%d8%b9%d9%87/%d9%be%db%8c%d8%b4%db%8c%d9%86%d9%87/" target="_blank" class="menu-link px-2">درباره</a>
-            </li>
-            <li class="menu-item">
-                <a href="https://nokarto.com/%d8%b1%d8%a7%d9%87-%d9%87%d8%a7%db%8c-%d8%a7%d8%b1%d8%aa%d8%a8%d8%a7%d8%b7%db%8c/" target="_blank" class="menu-link px-2">تماس</a>
+                <a href="https://nokarto.com/%d9%85%d8%ac%d9%85%d9%88%d8%b9%d9%87/%d9%be%db%8c%d8%b4%db%8c%d9%86%d9%87/" target="_blank" class="menu-link px-2">
+                    <img src="{{ URL::asset('front/img/nokarto_logo_colored.png') }}" height="35" alt="">
+                </a>
             </li>
         </ul>
         <!--end::Menu-->
