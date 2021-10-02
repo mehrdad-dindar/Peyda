bbbd9defe2cddb8c05e6f715570360041afa5dd9
Mehrdad Dindar <mehrdad.replay@gmail.com>
2021-10-03 02:42:43 +0330

    Complete Current day,week,month and year Sale Report

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,8 +14,39 @@ class HomeController extends Controller
         $this->middleware(['auth','verified','auth.usertype']);
     }
 
+    public function saleReportCurrent()
+    {
+        $todaySales = Transaction::whereDate('created_at', Carbon::today())->get();
+        $todaySum=0;
+        foreach ($todaySales as $todaySale){
+            $todaySum += $todaySale->paid;
+        }
+        $weekSales = Transaction::whereDate('created_at','>=', Carbon::now()->startOfWeek())->get();
+        $weekSum=0;
+        foreach ($weekSales as $weekSale){
+            $weekSum += $weekSale->paid;
+        }
+        $monthSales = Transaction::whereDate('created_at','>=', Carbon::now()->startOfMonth())->get();
+        $monthSum=0;
+        foreach ($monthSales as $monthSale){
+            $monthSum += $monthSale->paid;
+        }
+        $yearSales = Transaction::whereDate('created_at','>=', Carbon::now()->startOfYear())->get();
+        $yearSum=0;
+        foreach ($yearSales as $yearSale){
+            $yearSum += $yearSale->paid;
+        }
+        return [
+            'todaySum'      =>      $todaySum,
+            'weekSum'       =>      $weekSum,
+            'monthSum'      =>      $monthSum,
+            'yearSum'       =>      $yearSum,
+            ];
+    }
     public function index()
     {
-        return view('dashboard.home');
+        return view('dashboard.home')->with([
+            'current'       =>      $this->saleReportCurrent(),
+        ]);
     }
 }
